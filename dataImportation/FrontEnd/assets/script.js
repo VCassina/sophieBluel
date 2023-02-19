ac8f912360b8c5f69015037ae420cded226f63b8
VCassina <victorcassina@gmail.com>
2023-02-19 23:04:43 +0100

    Affichage dynamique avec boucle des IMG seulement (sens inverse)

--- dataImportation/FrontEnd/assets/script.js
+++ dataImportation/FrontEnd/assets/script.js
@@ -15,15 +15,15 @@ async function getData() {
 let arrayData;                      // Mise en place des data de l'API dans un tableau.
 getData().then(result => {          // Une fois que la function aura été executée, prend sa valeur de retour.
     arrayData = result;             // Et donne la au tableau arrayData (le JSON).
-    console.log(arrayData);         // Pour vérifier.
-    console.log(arrayData[0].imageUrl);
-    console.log(arrayData.length)
+    //console.log(arrayData);         
+    //console.log(arrayData[0].imageUrl);
+    //console.log(arrayData.length)
 
-    for (let i = arrayData.length - 1; i >= 0; i--) {
-        let galleryTargeting = document.querySelector(".gallery");
-        let galleryImage = document.createElement("img");
-        galleryImage.setAttribute("src", arrayData[i].imageUrl);
-        galleryTargeting.prepend(galleryImage);
+    for (let i = arrayData.length - 1; i >= 0; i--) {                   // Boucle qui affichera les images dans le sens inverse.
+        let galleryTargeting = document.querySelector(".gallery");      // Placement pour incorporation future.
+        let galleryImage = document.createElement("img");               // Création de la balise.
+        galleryImage.setAttribute("src", arrayData[i].imageUrl);        // Modification de l'attribut.
+        galleryTargeting.prepend(galleryImage);                         // L'incorporation.
     }
 
     // Ajouter le code à ajouter dynamiquement ici.
