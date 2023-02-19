a6591697d33f8dc6daa646fffc7720b15233dff2
VCassina <victorcassina@gmail.com>
2023-02-19 23:21:42 +0100

    Affichage dynamique IMG & TITLE (bouclé) mais soucis de balise <figure>

--- dataImportation/FrontEnd/assets/script.js
+++ dataImportation/FrontEnd/assets/script.js
@@ -15,16 +15,19 @@ async function getData() {
 let arrayData;                      // Mise en place des data de l'API dans un tableau.
 getData().then(result => {          // Une fois que la function aura été executée, prend sa valeur de retour.
     arrayData = result;             // Et donne la au tableau arrayData (le JSON).
-    //console.log(arrayData);         
+    console.log(arrayData);         
     //console.log(arrayData[0].imageUrl);
     //console.log(arrayData.length)
 
     for (let i = arrayData.length - 1; i >= 0; i--) {                   // Boucle qui affichera les images dans le sens inverse.
         let galleryTargeting = document.querySelector(".gallery");      // Placement pour incorporation future.
         let galleryImage = document.createElement("img");               // Création de la balise.
-        galleryImage.setAttribute("src", arrayData[i].imageUrl);        // Modification de l'attribut.
-        galleryTargeting.prepend(galleryImage);                         // L'incorporation.
+        let galleryTxt = document.createElement("figcaption");
 
+        galleryImage.setAttribute("src", arrayData[i].imageUrl);        // Modification de l'attribut.
+        galleryTxt.innerText = arrayData[i].title;
+
+        galleryTargeting.prepend(galleryImage, galleryTxt);             // L'incorporation.
         
     }
 })
