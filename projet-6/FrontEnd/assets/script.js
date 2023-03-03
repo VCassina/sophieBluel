d336d321c427785329588e6f06d6e3d8071586f2
VCassina <victorcassina@gmail.com>
2023-03-03 15:50:07 +0100

    Etape 2 terminée.

--- projet-6/FrontEnd/assets/script.js
+++ projet-6/FrontEnd/assets/script.js
@@ -70,28 +70,30 @@ function filterSelection(choose) {                                   // L'argume
 /* ___________________________________________________________ */
 /* Affiche les éléments.                                       */
 /* ___________________________________________________________ */
-function dataShow() {   
+function dataShow() {
     for (let i = arrayData.length - 1; i >= 0; i--) {                       // Boucle qui affichera les images dans le sens inverse.
 
         let galleryTargeting = document.querySelector(".gallery");
-        let galleryCard = document.createElement("figure");
-        let galleryImage = document.createElement("img");
-        let galleryTxt = document.createElement("figcaption");
+        if (galleryTargeting != null) {
+            let galleryCard = document.createElement("figure");
+            let galleryImage = document.createElement("img");
+            let galleryTxt = document.createElement("figcaption");
 
-        //console.log(arrayData[i].categoryId);
-        galleryCard.setAttribute("class", "figureCard " + arrayData[i].categoryId);     // Attribution d'une class aux arrayData.length cards (balises <figure>).         
-        galleryTargeting.prepend(galleryCard);                                          // Ajout des cards (balises <figure>).
+            //console.log(arrayData[i].categoryId);
+            galleryCard.setAttribute("class", "figureCard " + arrayData[i].categoryId);     // Attribution d'une class aux arrayData.length cards (balises <figure>).         
+            galleryTargeting.prepend(galleryCard);                                          // Ajout des cards (balises <figure>).
 
-        galleryImage.setAttribute("src", arrayData[i].imageUrl);            // Modification de l'attribut de la source img via l'API.
-        galleryImage.setAttribute("alt", arrayData[i].title);
+            galleryImage.setAttribute("src", arrayData[i].imageUrl);            // Modification de l'attribut de la source img via l'API.
+            galleryImage.setAttribute("alt", arrayData[i].title);
 
-        galleryTxt.innerText = arrayData[i].title;                          // Modification de le la description de l'img via l'API.
-        galleryTxt.setAttribute("class", "img_title");
+            galleryTxt.innerText = arrayData[i].title;                          // Modification de le la description de l'img via l'API.
+            galleryTxt.setAttribute("class", "img_title");
 
-        let InsideCardTargeting = document.querySelector(".figureCard");    // Préparation d'un placement dans les cards via la classe des balises <figure>.
-        InsideCardTargeting.prepend(galleryImage, galleryTxt);              // L'incorporation des deux sous-balises.
+            let InsideCardTargeting = document.querySelector(".figureCard");    // Préparation d'un placement dans les cards via la classe des balises <figure>.
+            InsideCardTargeting.prepend(galleryImage, galleryTxt);              // L'incorporation des deux sous-balises.
+        }
+        filterSelection("all");             // Attend l'importation pour lancer le premier filtre : "all".
     }
-    filterSelection("all");             // Attend l'importation pour lancer le premier filtre : "all".
 }
 
 /* ___________________________________________________________ */
@@ -128,7 +130,7 @@ console.log("The 'modal'-part of the script just started.")
 /* ___________________________________________________________ */
 /* Utilisation différente de "dataShow", adaptée à la modale ! */
 /* ___________________________________________________________ */
-function dataShowModal() {   
+function dataShowModal() {
     for (let i = arrayData.length - 1; i >= 0; i--) {                           // Boucle qui affichera les images dans le sens inverse.
 
         /* Déclaration des variables ! */
@@ -136,11 +138,11 @@ function dataShowModal() {
         let galleryCardEdit = document.createElement("figure");
         let galleryTxtEdit = document.createElement("a");
         let galleryIconeEdit = document.createElement("div");
-        let galleryIconeTrashEdit = document.createElement("i");              
-        let galleryIconeMoveEdit = document.createElement("i");                 
+        let galleryIconeTrashEdit = document.createElement("i");
+        let galleryIconeMoveEdit = document.createElement("i");
         let galleryImageEdit = document.createElement("img");
 
-       /* Gestion des attributs ! */
+        /* Gestion des attributs ! */
         galleryImageEdit.setAttribute("src", arrayData[i].imageUrl);            // Modification de l'attribut de la source img via les données importées de l'API.
         galleryImageEdit.setAttribute("alt", arrayData[i].title);
         galleryTxtEdit.innerText = "éditer";                                    // Ne renvoie à rien mais pourrait à l'avenir.
@@ -174,7 +176,7 @@ function dataShowModal() {
 function modalRemove() {
     /* Déclaration des variables à supprimer ! */
     let figureCardsToDeleteEdit = document.querySelectorAll(".edit_figureCard");        // Selection de toutes les classes .edit_figureCard.
-    figureCardsToDeleteEdit.forEach(function(e) {                                       // Remove ne peut être utilisé que sur un seul élément donc, pour chaque élément de figureCardsToDeleteEdit :
+    figureCardsToDeleteEdit.forEach(function (e) {                                      // Remove ne peut être utilisé que sur un seul élément donc, pour chaque élément de figureCardsToDeleteEdit :
         e.remove();                                                                     // Remove.
     });
 }
@@ -184,32 +186,106 @@ function modalRemove() {
 /* ___________________________________________________________ */
 const modalLinks = document.querySelectorAll('a[href="#modalBox"]'); // Tous les liens (a) avec href qui comporte notre ancrage.
 modalLinks.forEach(link => {                                         // Ecoute chaque clique sur ces deux lien.
-  link.addEventListener("click", (event) => {
-    event.preventDefault();                                          // On ne veut pas un fonctionnement de l'ancrage.
-    const modalBox = document.getElementById("modalBox");            // modalBox est notre élément comportement l'ID modalBox.
-    modalBox.classList.remove("modalBox-hidden");                    // On lui retire la modalBox-hidden, ce qui le révèle. 
-    modalBox.removeAttribute("aria-hidden");                         // Gestion des balises liées à l'accesibilité pour personnes mal-voyantes.
-    modalBox.setAttribute("aria-modal", "true");                     // //
-    dataShowModal();
-});
+    link.addEventListener("click", (event) => {
+        event.preventDefault();                                          // On ne veut pas un fonctionnement de l'ancrage.
+        const modalBox = document.getElementById("modalBox");            // modalBox est notre élément comportement l'ID modalBox.
+        modalBox.classList.remove("modalBox-hidden");                    // On lui retire la modalBox-hidden, ce qui le révèle. 
+        modalBox.removeAttribute("aria-hidden");                         // Gestion des balises liées à l'accesibilité pour personnes mal-voyantes.
+        modalBox.setAttribute("aria-modal", "true");                     // //
+        dataShowModal();
+    });
 });
 
 /* ___________________________________________________________ */
 /* Fermeture de la modale !                                    */
 /* ___________________________________________________________ */
-function closeModal() {                                  
+function closeModal() {
     modalBox.classList.add("modalBox-hidden");                    // On lui remet la modalBox-hidden, ce qui le cache. 
     modalBox.setAttribute("aria-hidden", "true");                 // Gestion des balises liées à l'accesibilité pour personnes mal-voyantes.
     modalBox.setAttribute("aria-modal", "false");                 // //                                             // La modale n'est plus ouverte.
     modalRemove();                                                // Supprimer le contenu DOM générer pour pouvoir rouvrir la modale sans avoir une accumulation.
-    console.log("modalRemove has been executed !");
 }
 
 /* ___________________________________________________________ */
 /* Fermeture de la modale possible au click de la croix.       */
 /* ___________________________________________________________ */
 const modalCross = document.querySelector(".fa-xmark");
-modalCross.addEventListener('click', () => {
-    closeModal();
-})
+if (modalCross != null) {
+    modalCross.addEventListener('click', () => {
+        closeModal();
+    });
+}
 console.log("The script just ended.")
+
+/* ___________________________________________________________ */
+/* Formulaire.                                                 */
+/* ___________________________________________________________ */
+let tokenSaved = "";
+
+/* ___________________________________________________________ */
+/* Envoie des ID et attente de réponse.                        */
+/* ___________________________________________________________ */
+async function postData(url = "", data = {}) {              // Function async ayant besoin d'une URL et de données. 
+    const response = await fetch(url, {                       // une réponse sous forme de constante est attendue.
+        method: "POST",                                         // Le fetch initié est en méthode POST.
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),                          // Les données de "data" sont stringifiées en JSON avant d'être envoyées.
+    });
+
+    const responseJSON = await response.json();            // Attente de notre réponse en .JSON de l'API et stockage de son contenu.
+    tokenSaved = responseJSON.token;                       // Stocker le token de réponse dans la variable "token"
+    return responseJSON;                                   // On return notre constante qui fait la demande et reçoit la réponse comme résultat de la function.
+}
+
+let form = document.getElementById('login_form');     // Selection de notre formulaire.
+if (form != null) {                                   // Si l'ID "form" correspond à qq chose, alors :
+    form.addEventListener("submit", (ev) => {             // Si on clique sur Submit avec l'argument étant le contenu du form !
+        ev.preventDefault();                              // N'actualise pas la page quand on clique.
+        let data = new FormData(ev.target);               // Création d'un objet "data" qu'on vient remplir avec le contenu de la cible, à savoir lui même, en gros : Envoie du contenu du formulaire dans "data".
+        let user = {                                      // Nouvel objet user qui vient recevoir pour email le contenu de la balise "email_login" de l'objet "data" et idem pour le password.
+            email: data.get('email_login'),
+            password: data.get('password_login')
+        };
+
+        let errorField = document.querySelector(".errorEmptyField");    // S'il y a déjà un message d'erreur car le formulaire n'est pas correctement reseigné.
+        if (errorField != null) {                                       // Si l'élément est trouvé, alors :
+            errorField.parentNode.removeChild(errorField);              // On supprime l'élément du DOM.
+        }
+
+        if (user.email.trim() !== '' && user.password.trim() !== '') {                // trim permet de valider une chaine de charactère vide, cela évite les erreurs d'interprétations de "false".
+            postData('http://127.0.0.1:5678/api/users/login', user).then(data => {    // Ensuite, appelle de la fonction postData avec l'URL de l'API et nos données de formulaire en argument.
+                console.log(data);                                                    // Vérification du bon contenu de "data".
+                console.log(tokenSaved);                                              // Vérification du bon contenu du token !
+                                                                                      // S'il est renseigné un champ email et MDP, il ne peut y avoir que deux cas de figure :
+            /* ___________________________________________________________ */
+            /* Dans le cas où l'API ne retourne pas d'erreur :             */
+            /* ___________________________________________________________ */
+            if (data.userId == 1) {
+                window.location.href = '../pages/index_edit.html';
+            }
+
+            /* ___________________________________________________________ */
+            /* Dans le cas où l'API retourne une erreur :                  */
+            /* ___________________________________________________________ */
+            else {
+                let link = document.querySelector("#button_login");
+                let p = document.createElement("p");
+                p.setAttribute("class", "errorEmptyField")
+                let textError = document.createTextNode("Les informations ne correspondent pas.");
+                p.appendChild(textError);
+                link.parentNode.insertBefore(p, link);
+            }
+            })
+
+        } else {                                                                      // Et sinon, on insère en DOM à l'utilisateur qu'il doit remplir tous les champs !
+            let link = document.querySelector("#button_login");
+            let p = document.createElement("p");
+            p.setAttribute("class", "errorEmptyField")
+            let textError = document.createTextNode("Veuillez remplir tous les champs.");
+            p.appendChild(textError);
+            link.parentNode.insertBefore(p, link);
+        }
+    })
+};
