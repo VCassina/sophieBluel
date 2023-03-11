f6fb8bafafd8a902238b32c782c7d245ffe0b334
VCassina <victorcassina@gmail.com>
2023-03-11 14:55:36 +0100

    Reception ET stockage locale des informations du formulaire - SANS IMAGE pour le moment.

--- projet-6/FrontEnd/assets/script.js
+++ projet-6/FrontEnd/assets/script.js
@@ -362,7 +362,7 @@ modalLink.forEach(link => {
                 }
             });
         let modalBoxHitBox = document.querySelector('#modalBox');       // On séléctionne la modale.
-        document.addEventListener('click', (event) => {                 // On écoute les clicks qui ont lieu sur TOUTE la page.
+        document.addEventListener('click', (event) => {                 // On écoute les clicks qui ont lieu sur TOUTE la page / BODY est couvert par la modal ? Body ne marche pas.
             if (event.target === modalBoxHitBox) {                      // Si cela est exacte, il s'agit de la fenêtre modale MAIS :
                 //
                 // #modalBox fait référence à l'entierté de la page car la modale prend toute la place.
@@ -407,7 +407,7 @@ function closeModal() {
 /* Supprimer quelque chose dans la modale.                     */
 /* ___________________________________________________________ */
 /* SUPPRESSION FONCTIONNE mais en peux traiter trop de demandes, pourquoi !? => Voir ligne 411 !! */
-let pendingRequests = []; // A ne pas déclarer dans la fonction car le code lu trouve un eventListener avant l'appel de la fonction et une erreur apparait !
+let requestToDelete = []; // A ne pas déclarer dans la fonction car le code lu trouve un eventListener avant l'appel de la fonction et une erreur apparait !
 function removePictureListening() {                              
     let trashCan = document.querySelectorAll('.fa-trash-can');              // Selectionne nos trashcans.     
     let trashCanId = [];                                                    // Va permettre de lier nos ID et nos index pour faire correspondre les trashcans aux images séléctionnées.
@@ -419,7 +419,7 @@ function removePictureListening() {
 
       trashCan.addEventListener('click', () => {                            // Lorsqu'on clique sur une icone trashcan.              
             let idToDelete = trashCanId[index];                             // L'icone exacte sur laquelle est on à cliqué est numérotée et renseignée dans idToDelete.
-            pendingRequests.push({                                          // On ajoute également, en agrandissant le tableau pendingRequests, une requête fetch qu'on enverra plus tard.
+            requestToDelete.push({                                          // On ajoute également, en agrandissant le tableau requestToDelete, une requête fetch qu'on enverra plus tard.
               method: 'DELETE',
               url: "http://localhost:5678/api/works/" + idToDelete,                     // La demande touche à notre idtoDelete, là où nous avons cliqué.
               headers: { 'Authorization': "Bearer " + getTokenCookie("loginToken") }    // Ne pas oublier le token pour se faire accepter par la demande.
@@ -436,7 +436,7 @@ function removePictureListening() {
 }
 
 /* ___________________________________________________________ */
-/* ACTION - Listeners de "supprimer la galerie".               */
+/* ACTION - Listeners de "supprimer la galerie" - LOCALEMENT ! */
 /* ___________________________________________________________ */
 const galleryDelete = document.querySelector('#gallery_delete'); // Selection de tous nos éléments avec l'id.
 if (galleryDelete) {                                             // Pour eviter les erreurs consoles (car je n'utilise qu'un seul script).
@@ -464,8 +464,8 @@ galleryDelete.addEventListener('click', () => {
 changementApplyButton = document.getElementById("changementApply");
 if (changementApplyButton) {
 document.getElementById("changementApply").addEventListener("click", () => {
-    // On exécute toutes les requêtes en attente et stockées dans pendingRequests > Méthode de stackOverflow.
-    Promise.all(pendingRequests.map(request => fetch(request.url, { method: request.method, headers: request.headers })))
+    // On exécute toutes les requêtes en attente et stockées dans requestToDelete > Méthode de stackOverflow.
+    Promise.all(requestToDelete.map(request => fetch(request.url, { method: request.method, headers: request.headers })))
       .then(responses => {                                                                                                      // On attend la réponse.
         if (responses.every(response => response.ok)) {                                                                         // Si elle est correct :
           setTimeout(() => {                                                                                                    // Pour laisser le temps aux demandes, inutile ? - Oui, je pense.
@@ -494,7 +494,7 @@ if (secondModalButton) {                                                        
         
 
         isSecondModalOpen = true;                                              // Savoir quand la modale est ouverte et fermée (sert plus tard).
-        console.log(isSecondModalOpen + " < SECONDE MODALE !");
+        console.log(isSecondModalOpen + " < seconde modale !");
         
         /* ___________________________________________________________              */
         /* Fermeture de la modale possible au click de la croix + hors cadre & ESC. */
@@ -502,10 +502,8 @@ if (secondModalButton) {                                                        
         // Désormais ici car rajout du boolean (pour mieux suivre ET considérer le clique en dehors de la modale) !
 
         if (isSecondModalOpen === true) {                                       // Si la SECONDE fenêtre modale est ouverte :
-            console.log("Sortez moi de là.");                                   // Est appliqué correctement (jusqu'à présent).
             let modalCross = document.querySelector(".fa-xmarkOfSecondModal");               // On identifie la croix.
             modalCross.addEventListener('click', () => {                                     // Alors on place notre eventListener sur le clique.
-                console.log("Clique correctement reçu.");
                 closeSecondModal();                                                          // Et ça viendra fermer la modale qui est désormais la seule active.
 
             })
@@ -514,9 +512,12 @@ if (secondModalButton) {                                                        
                     closeSecondModal();
                 }
             });
-      
-        let modalBoxHitBox = document.querySelector("#modalBoxAddPicture");     // On séléctionne notre deuxieme modale dans le HTML.
-        document.addEventListener('click', (event) => {                         // On écoute les cliques sur toute la page.
+
+            addingPictureFormInformation();
+            console.log("I'm listening.");
+            
+            let modalBoxHitBox = document.querySelector("#modalBoxAddPicture");     // On séléctionne notre deuxieme modale dans le HTML.
+            document.addEventListener('click', (event) => {                         // On écoute les cliques sur toute la page.
             if (event.target === modalBoxHitBox) {                              
         //
         // #modalBox fait référence à l'entierté de la page car la modale prend toute la place.
@@ -532,9 +533,7 @@ if (secondModalButton) {                                                        
 /* ___________________________________________________________ */
 /* Fermeture de la seconde modale.                             */
 /* ___________________________________________________________ */
-
 function closeSecondModal () {
-
     let secondModalBox = document.getElementById("modalBoxAddPicture");                     // modalBox est notre élément comportement l'ID modalBox.
     secondModalBox.classList.add("modalBox-hidden");                                        // On a rien vu, on remet comme c'était avant l'ouverture.
     secondModalBox.setAttribute("aria-hidden", "true");                                     // 
@@ -561,7 +560,122 @@ if (secondModalBackButton) {                                                    
                 removePictureListening();                                        // Applique une mise en écoute des corbeilles pour suppression.
                 isMainModalOpen = true;    
             })
-        };
+};
+
+/* ___________________________________________________________ */
+/* Uploade une image.                                          */
+/* ___________________________________________________________ */
+// 1 - Récupérer les données du formulaire avant même de penser à l'envoyer.
+/* Les informations à récupérer sont les suivantes :
+{
+  "id": 0,
+  "title": "string",
+  "imageUrl": "string",
+  "categoryId": "string",
+  "userId": 0
+} 
+*/
+let addingPictureForm = {                                   // Tableau au format de ce que je vais devoir envoyer en fetch.
+    id: 0,
+    title: "",
+    imageUrl: "",
+    categoryId: "",
+    userId: 0
+};
+
+const pictureList = [];                                     // Tableau pour stocker mes "sessions" d'addingPictureForm, me permettant de stocker les infos des images que je vais ensuite envoyer en fetch.
+
+function addPictureListeningToList(title, categoryId) {     // Ajout des informations rentrées dans le tableau pictureList.
+    addingPictureForm = {
+    id: arrayData.length + pictureList.length,
+    title: title,
+    imageUrl: "",
+    categoryId: categoryId,
+    userId: 0
+  };
+
+  pictureList.push(addingPictureForm);
+  console.log("Formulaire ajouté à la liste :", addingPictureForm, ".");
+  console.log("En tout, voila ce que je vais envoyer en fetch : ", pictureList, ".");
+}
+
+function addingPictureFormInformation () {
+let addPictureForm = document.querySelector("#pictureAdd");               // Le formulaire (pour écouter sa validation).
+let addPictureTitle = addPictureForm.querySelector("#titlePictureAdd");                 // La valeur titre.
+let addPictureCategory = addPictureForm.querySelector('#categoryPictureAdd');           // La valeur id (catégorie).
+let addPictureImage = addPictureForm.querySelector('');
+addPictureForm.addEventListener("submit", (event) => {                                    // On écoute le questionnaire en cas de validation.
+    event.preventDefault();
+    let errorInformationModale = document.querySelector(".errorSecondModale");            // Si un message d'erreur est trouvé, est déjà là.
+    if (errorInformationModale) {
+        errorInformationModale.remove();                                                  // Il est supprimé. Plus simple que pour login.
+    }
+    switch (true) {                                                                       // Conditionnement qui se lance de base.
+        case !addPictureTitle.value || !addPictureCategory.value:                         // Dans le cas où il manque l'un ou l'autre.
+
+          let link = document.querySelector("#pictureAddConformation");                   // Affichage du message d'erreur, repris sur le login.
+          let p = document.createElement("p");
+          p.setAttribute("class", "errorSecondModale")
+          let textError = document.createTextNode("Veuillez remplir tous les champs.");
+          p.appendChild(textError);
+          link.parentNode.insertBefore(p, link);
+
+          break;
+        case addPictureTitle.value.length > 180:                                          // Nombre de charactère max pour le titre (arbitraire ici).
+
+          let link2 = document.querySelector("#pictureAddConformation");                  // Affichage du message d'erreur, repris sur le login.
+          let p2 = document.createElement("p");
+          p2.setAttribute("class", "errorSecondModale")
+          let textError2 = document.createTextNode("Le titre est trop long (180 chars max).");
+          p2.appendChild(textError2);
+          link2.parentNode.insertBefore(p2, link2);
+
+
+          break;
+        case !/^[A-Za-z0-9\s]+$/.test(addPictureTitle.value):                           // ASCII art uniquement - StackOverflow.
+
+
+          let link3 = document.querySelector("#pictureAddConformation");                // Affichage du message d'erreur, repris sur le login.
+          let p3 = document.createElement("p");
+          p3.setAttribute("class", "errorSecondModale")
+          let textError3 = document.createTextNode("Un ou plusieurs charactères spéciaux posent problèmes.");
+          p3.appendChild(textError3);
+          link3.parentNode.insertBefore(p3, link3);
+
+          break;
+        case !/^[A-Z]/.test(addPictureTitle.value):                                     // Maj uniquement - StackOverflow.
+
+          let link4 = document.querySelector("#pictureAddConformation");                // Affichage du message d'erreur, repris sur le login.
+          let p4 = document.createElement("p");
+          p4.setAttribute("class", "errorSecondModale")
+          let textError4 = document.createTextNode("Veuillez commencez votre titre par une lettre majuscule.");
+          p4.appendChild(textError4);
+          link4.parentNode.insertBefore(p4, link4);
+          break;
+
+        default:
+          console.log("Validation du formulaire.");                                 // Si tout est bon, dans le cas où, en tout cas, on ne trouve pas d'erreur pour le moment :
+          addingPictureForm.title = addPictureTitle.value;                          // On fait correspondre les valeurs renseignées dans le formulaire avec le tableau qui va servir,
+          addingPictureForm.categoryId = addPictureCategory.value;                  // pour renseigner à l'API nos informations.
+          console.log("Titre :", addingPictureForm.title);
+          console.log("Catégorie :", addingPictureForm.categoryId);
+          addPictureListeningToList(addPictureTitle.value, addPictureCategory.value);
+          break;
+        }
+    });
+  }
+
+/*
+1 - Faire une liste des changements en LOCALE (comme pour le delete).*/
+
+
+
+
+/*
+        - Etablir un tableau, le renseigner, lors de chaque renseignement, actualisation locale.
+2 - Envoyer l'upload dans l'API pour confirmer ces changements en ligne.
+*/
+
 
 console.log("The script just ended.");
   
